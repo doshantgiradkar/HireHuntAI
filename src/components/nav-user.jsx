1ef79d5a0c518225eebf507a15b27b5ab3aaa467
devmulkalwar <devmulkalwar95@gmail.com>
2025-07-20 14:23:40 +0530

    logout functionality added

--- src/components/nav-user.jsx
+++ src/components/nav-user.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import {
   IconCreditCard,
@@ -6,13 +6,9 @@ import {
   IconLogout,
   IconNotification,
   IconUserCircle,
-} from "@tabler/icons-react"
+} from "@tabler/icons-react";
 
-import {
-  Avatar,
-  AvatarFallback,
-  AvatarImage,
-} from "@/components/ui/avatar"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -21,22 +17,21 @@ import {
   DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
+} from "@/components/ui/dropdown-menu";
 import {
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
   useSidebar,
-} from "@/components/ui/sidebar"
-import { useSession } from "@clerk/nextjs"
+} from "@/components/ui/sidebar";
+import { SignOutButton, UserButton, useSession } from "@clerk/nextjs";
 
 export function NavUser() {
-  const { session } = useSession()
-  const user = session?.user
-  const { isMobile } = useSidebar()
-  
+  const { session } = useSession();
+  const user = session?.user;
+  const { isMobile } = useSidebar();
 
-  if (!user) return null
+  if (!user) return null;
 
   return (
     <SidebarMenu>
@@ -45,15 +40,27 @@ export function NavUser() {
           <DropdownMenuTrigger asChild>
             <SidebarMenuButton
               size="lg"
-              className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground">
+              className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+            >
               <Avatar className="h-8 w-8 rounded-lg grayscale">
-                <AvatarImage src={user.imageUrl} alt={user.fullName || user.username || user.emailAddresses[0]?.emailAddress} />
+                <AvatarImage
+                  src={user.imageUrl}
+                  alt={
+                    user.fullName ||
+                    user.username ||
+                    user.emailAddresses[0]?.emailAddress
+                  }
+                />
                 <AvatarFallback className="rounded-lg">
                   {user.fullName ? user.fullName[0] : "U"}
                 </AvatarFallback>
               </Avatar>
               <div className="grid flex-1 text-left text-sm leading-tight">
-                <span className="truncate font-medium">{user.fullName || user.username || user.emailAddresses[0]?.emailAddress}</span>
+                <span className="truncate font-medium">
+                  {user.fullName ||
+                    user.username ||
+                    user.emailAddresses[0]?.emailAddress}
+                </span>
                 <span className="text-muted-foreground truncate text-xs">
                   {user.emailAddresses[0]?.emailAddress}
                 </span>
@@ -65,23 +72,36 @@ export function NavUser() {
             className="w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg"
             side={isMobile ? "bottom" : "right"}
             align="end"
-            sideOffset={4}>
+            sideOffset={4}
+          >
             <DropdownMenuLabel className="p-0 font-normal">
               <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                 <Avatar className="h-8 w-8 rounded-lg">
-                  <AvatarImage src={user.imageUrl} alt={user.fullName || user.username || user.emailAddresses[0]?.emailAddress} />
+                  <AvatarImage
+                    src={user.imageUrl}
+                    alt={
+                      user.fullName ||
+                      user.username ||
+                      user.emailAddresses[0]?.emailAddress
+                    }
+                  />
                   <AvatarFallback className="rounded-lg">
                     {user.fullName ? user.fullName[0] : "U"}
                   </AvatarFallback>
                 </Avatar>
                 <div className="grid flex-1 text-left text-sm leading-tight">
-                  <span className="truncate font-medium">{user.fullName || user.username || user.emailAddresses[0]?.emailAddress}</span>
+                  <span className="truncate font-medium">
+                    {user.fullName ||
+                      user.username ||
+                      user.emailAddresses[0]?.emailAddress}
+                  </span>
                   <span className="text-muted-foreground truncate text-xs">
                     {user.emailAddresses[0]?.emailAddress}
                   </span>
                 </div>
               </div>
             </DropdownMenuLabel>
+
             <DropdownMenuSeparator />
             <DropdownMenuGroup>
               <DropdownMenuItem>
@@ -98,13 +118,15 @@ export function NavUser() {
               </DropdownMenuItem>
             </DropdownMenuGroup>
             <DropdownMenuSeparator />
-            <DropdownMenuItem onClick={() => logout()}>
-              <IconLogout />
-              Log out
-            </DropdownMenuItem>
+            <SignOutButton>
+              <DropdownMenuItem>
+                <IconLogout />
+                Log out
+              </DropdownMenuItem>
+            </SignOutButton>
           </DropdownMenuContent>
         </DropdownMenu>
       </SidebarMenuItem>
     </SidebarMenu>
-  )
+  );
 }
--- src/middleware.js
+++ src/middleware.js
@@ -2,7 +2,7 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 import { NextResponse } from 'next/server';
 
-const isPublicRoute = createRouteMatcher(['/login', '/sign-in']);
+const isPublicRoute = createRouteMatcher(['/login', '/sign-in',"/"]);
 const isSelectRoleRoute = createRouteMatcher(['/select-role']);
 const isCandidateRoute = createRouteMatcher(['/candidate', '/candidate/(.*)']);
 const isRecruiterRoute = createRouteMatcher(['/recruiter', '/recruiter/(.*)']);
