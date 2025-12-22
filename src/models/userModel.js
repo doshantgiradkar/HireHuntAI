fd55a9dedaedbdda7bac9e0a09fe887e7aab4674
Doshant Giradkar <gdoshant@gmail.com>
2025-12-22 11:05:25 +0530

    Changed User Model to camelCase ;)

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,13 +1,21 @@
 import mongoose, { SchemaTypes } from "mongoose";
 
-const userSchema = new mongoose.Schema({
-    clerk_id: { type: String, required: true },
-    first_name: { type: String, required: [true, "Please add first name"] },
-    last_name: { type: String, required: [true, "Please add last name"] },
-    email: { type: String, required: [true, "Please enter email ID"], lowercase: true, unique: true },
-    image_url: { type: String, required: false },
-    role: { type: String, enum: ['recruiter', 'candidate'], required: false },
-}, { timestamps: true });
+const userSchema = new mongoose.Schema(
+  {
+    clerkId: { type: String, required: true },
+    firstName: { type: String, required: [true, "Please add first name"] },
+    lastName: { type: String, required: [true, "Please add last name"] },
+    email: {
+      type: String,
+      required: [true, "Please enter email ID"],
+      lowercase: true,
+      unique: true,
+    },
+    imageUrl: { type: String, required: false },
+    role: { type: String, enum: ["recruiter", "candidate"], required: false },
+  },
+  { timestamps: true },
+);
 
 const User = mongoose.models.User || mongoose.model("User", userSchema);
 export default User;
